d4b3fdbaa797e308442353b2fe1f0e420f987b9d
roniahamed <mdroniahamed56@gmail.com>
2025-04-24 17:13:15 +0600

    adding new feature: search and pagination

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Products 
+from django.core.paginator import Paginator
 
 
 def index(request):
-    product_objects = Products.objects.all()
+    product_objects = Products.objects.only('id')
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
         product_objects = Products.objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_objects, 100)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
     return render(request, 'shop/index.html', {'product_objects':product_objects})
