039282330fc103bd9b7af0a95766a3ff23a2ebec
roniahamed <mdroniahamed56@gmail.com>
2025-04-24 13:11:41 +0600

    adding search feauture views in views.py

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,7 @@ from .models import Products
 
 def index(request):
     product_objects = Products.objects.all()
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = Products.objects.filter(title__icontains=item_name)
     return render(request, 'shop/index.html', {'product_objects':product_objects})
